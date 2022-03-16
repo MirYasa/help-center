2478d96b988c23b3714d1d07127148302f9f96d1
MirYasa <gurevich.miron14@gmail.com>
2022-03-16 18:44:32 +0300

    Fix failed

--- blog/new4.md
+++
@@ -1,8 +0,0 @@
----
-title: NEW
-date: 2022-02-02
-category: getting-start
-slug: new4
----
-
-ashgfhjasjflasjnfjnsajnkfkasfkbjasbjkfbjkasfkbjasjbkfbjkasbfjk
--- src/components/articles/articles.tsx
+++ src/components/articles/articles.tsx
@@ -22,25 +22,33 @@ export const query = graphql`
 
 export default function Articles({data, location}: any) {
     const title = useMemo(() => {
-        return data.allMarkdownRemark.nodes[0].frontmatter.category.split('-').map((el: string) => el.charAt(0).toUpperCase() + el.slice(1)).join(' ')
+        return data.allMarkdownRemark.nodes[0]?.frontmatter.category.split('-').map((el: string) => el.charAt(0).toUpperCase() + el.slice(1)).join(' ')
     }, [])
+
+    console.log(data)
 
     return (
         <>
             <Header location={location} label={title}/>
             <div className={'page-container articles'}>
-                <h1>{title}</h1>
-                <ul className={'articles__list'}>
-                    {data.allMarkdownRemark.nodes.map((el: any) =>
-                        <li key={el.id}>
-                            <Link className={'articles__link'} to={el.frontmatter.slug}>
-                                <h3>{el.frontmatter.title}</h3>
-                                <p>{el.frontmatter.date}</p>
-                            </Link>
-                        </li>
-                    )
-                    }
-                </ul>
+                {
+                    data.allMarkdownRemark.nodes.length !== 0 ? (
+                        <>
+                            <h1>{title}</h1>
+                            <ul className={'articles__list'}>
+                                {data.allMarkdownRemark.nodes.map((el: any) =>
+                                    <li key={el.id}>
+                                        <Link className={'articles__link'} to={el.frontmatter.slug}>
+                                            <h3>{el.frontmatter.title}</h3>
+                                            <p>{el.frontmatter.date}</p>
+                                        </Link>
+                                    </li>
+                                )
+                                }
+                            </ul>
+                        </>
+                    ) : <div>No articles in this category</div>
+                }
             </div>
             <Footer/>
         </>
